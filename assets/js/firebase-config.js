20c26aaf5fe8da1ed40dbcdc2bad2781b0a315e8
Sridatta <sridatta.bharadwaj2006@gmail.com>
2025-03-13 14:26:49 +0530

    formatted files

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -4,15 +4,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 export // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBcCsYA5wKqoX-o_lKM0QyRVV3608NSVAs",
-  authDomain: "blog-test-fc1f6.firebaseapp.com",
-  projectId: "blog-test-fc1f6",
-  storageBucket: "blog-test-fc1f6.firebasestorage.app",
-  messagingSenderId: "146015794221",
-  appId: "1:146015794221:web:dd99d5b4b1ad5fb3c634a4",
-  measurementId: "G-ZRB9SZNZ6W"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBcCsYA5wKqoX-o_lKM0QyRVV3608NSVAs",
+    authDomain: "blog-test-fc1f6.firebaseapp.com",
+    projectId: "blog-test-fc1f6",
+    storageBucket: "blog-test-fc1f6.firebasestorage.app",
+    messagingSenderId: "146015794221",
+    appId: "1:146015794221:web:dd99d5b4b1ad5fb3c634a4",
+    measurementId: "G-ZRB9SZNZ6W"
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
